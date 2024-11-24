ff9ddbc504d2a7c3e3e314189c5beb6c01ec725b
Jiaming Li <dvd.li@mail.utoronto.ca>
2024-11-24 18:37:12 -0500

    no conflict

---
+++ PP2/scriptorium/utils/auth.ts
@@ -0,0 +1,22 @@
+// utils/auth.ts
+
+// Save the JWT token to localStorage
+export const saveToken = (token: string): void => {
+    localStorage.setItem("token", token);
+  };
+  
+  // Retrieve the JWT token from localStorage
+  export const getToken = (): string | null => {
+    return localStorage.getItem("token");
+  };
+  
+  // Remove the JWT token from localStorage
+  export const removeToken = (): void => {
+    localStorage.removeItem("token");
+  };
+  
+  // Check if the user is authenticated by verifying token existence
+  export const isAuthenticated = (): boolean => {
+    return !!getToken();
+  };
+  
---
+++ PP2/scriptorium/utils/theme.ts
@@ -0,0 +1,14 @@
+export const setTheme = (theme: "light" | "dark") => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  };
+  
+  export const getTheme = (): "light" | "dark" => {
+    return (localStorage.getItem("theme") as "light" | "dark") || "light";
+  };
+  
+  export const toggleTheme = () => {
+    const currentTheme = getTheme();
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
